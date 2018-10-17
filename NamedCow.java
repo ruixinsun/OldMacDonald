2c469c21ce8f40de6f1c458e13588c42e339737b
ruixinsun <42555093+ruixinsun@users.noreply.github.com>
2018-10-17 13:35:30 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,10 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String myName;
+  NamedCow(String name)
+  {
+    myName = name;
+    
+  }
+  public String getName(){return myName;} 
 }
